b4ce6e5431fc91bb99d15a9d4df52e0dbbcf9253
Ryan Trattner <ryantrattner@gmail.com>
2019-05-27 23:33:39 -0700

    wow

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var app = express();
 var path = require('path');
 
 //app.use(express.static(__dirname)); // Current directory is root
-app.use(express.static(__dirname)); //  "public" off of current is root
+app.use(express.static(__dirname + "/new/webapp")); //  "public" off of current is root
 
 app.listen(3000);
 console.log('Listening on port 3000');
