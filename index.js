e2a60079b41d0fe6c7e6872444a0bfdc6334e372
Ryan Trattner <ryantrattner@gmail.com>
2019-05-27 21:44:03 -0700

    Update index.js

--- index.js
+++ index.js
@@ -5,5 +5,5 @@ var path = require('path');
 //app.use(express.static(__dirname)); // Current directory is root
 app.use(express.static(__dirname)); //  "public" off of current is root
 
-app.listen(80);
-console.log('Listening on port 80');
+app.listen(3000);
+console.log('Listening on port 3000');
